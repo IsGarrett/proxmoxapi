873391ec7c5276faa76e36efdefb04f28ab3be05
IsGarrett <166757512+IsGarrett@users.noreply.github.com>
2026-05-21 15:03:52 -0600

    Added endpoint ability to accept form data for UI logins.

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -4,6 +4,7 @@ from db import get_db
 from models.user import User
 from schemas.auth import UserRegister, TokenResponse, UserLogin
 from services.auth import hash_password, verify_password, create_access_token, decode_token
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
@@ -32,19 +33,17 @@ def auth_register(user: UserRegister, db: Session = Depends(get_db)):
     return {"message": "User registered successfully", "username": new_user.username}
 
 
-@router.post('/v1/auth/login')
-def user_login(user:UserLogin, db:Session = Depends(get_db)):
 
-    existing_username = db.query(User).filter(User.username == user.username).first()
+
+@router.post('/v1/auth/login')
+def user_login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    existing_username = db.query(User).filter(User.username == form_data.username).first()
 
     if not existing_username:
-        raise HTTPException(status_code=401, detail="invalid username or password")
+        raise HTTPException(status_code=401, detail="Invalid username or password")
     
+    if not verify_password(form_data.password, existing_username.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid username or password")
 
-    if not verify_password(user.password, existing_username.hashed_password):
-        raise HTTPException(status_code=401, detail='Invalid username or password')
-
-
-    token_generation = create_access_token({"sub": user.username})
-
-    return TokenResponse(access_token=token_generation, token_type='bearer')
+    token = create_access_token({"sub": existing_username.username})
+    return TokenResponse(access_token=token, token_type="bearer")
