59e3db889f9406f3d475107789573631bd89fff8
IsGarrett <166757512+IsGarrett@users.noreply.github.com>
2026-05-20 19:55:09 -0600

    Fixed model schema for celery worker

--- models/lxc_metric.py
+++ models/lxc_metric.py
@@ -7,6 +7,7 @@ class LxcMetric(Base):
     __tablename__ = 'lxc_metrics'
 
     id = Column(BigInteger, primary_key=True, index=True)
+    vmid = Column(Integer)
     name = Column(String, nullable=False)
     status = Column(String)
     cpu = Column(Float)
